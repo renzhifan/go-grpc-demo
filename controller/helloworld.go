13fe87734017d0c4d5a219e1853c1d35224b2ea4
renzhifan <renzhifan@haohaozhu.com>
2023-01-13 14:26:36 +0800

    验证接口实现 并添加注释

--- controller/helloworld.go
+++ controller/helloworld.go
@@ -11,6 +11,10 @@ type HelloworldService struct {
 	pb.UnimplementedGreeterServer
 }
 
+// 判断 GreeterServer 是否实现了 helloworld.proto.SayHello
+// 不要问为什么这么写，就好像「地球为什么是椭圆的」
+var _ pb.GreeterServer = (*HelloworldService)(nil)
+
 // SayHello implements helloworld.GreeterServer
 func (s *HelloworldService) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	log.Printf("Received: %v", in.GetName())
--- controller/passport.go
+++ controller/passport.go
@@ -11,6 +11,10 @@ type PassportService struct {
 	pb.UnimplementedMemberServer
 }
 
+// 判断 MemberServer 是否实现了 passport.proto.GetMemberProfileByUid
+// 不要问为什么这么写，就好像「地球为什么是椭圆的」
+var _ pb.MemberServer = (*PassportService)(nil)
+
 // GetMemberProfileByUid implements helloworld.MemberServer
 func (s *PassportService) GetMemberProfileByUid(ctx context.Context, in *pb.GetMemberProfileByUidReq) (*pb.GetMemberProfileByUidReply, error) {
 	log.Printf("Greeting:uid %d,option:%d", in.GetUid(), in.GetOption())
